9606ee1eb509dd162783a2892c51542b60effa33
Katherine González A. <54302755+Katherine506@users.noreply.github.com>
2024-09-22 00:14:06 -0600

    category y product

---
+++ src/main/java/com/project/demo/logic/entity/product/Product.java
@@ -0,0 +1,66 @@
+package com.project.demo.logic.entity.product;
+
+import com.project.demo.logic.entity.category.Category;
+import jakarta.persistence.*;
+
+@Table(name = "product")
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    private String name;
+    private String description;
+    private double price;
+    private int stock;
+    private Category category;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public Category setCategory(Category category) {
+        this.category = category;
+    }
+
+}
